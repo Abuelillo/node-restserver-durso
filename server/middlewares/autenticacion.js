efd13002df42b72c0764c36486b45d4576b8746f
Juan <juanvarios32@gmail.com>
2020-08-17 05:04:50 -0700

    arreglo

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -7,21 +7,22 @@ let verificaToken = (req, res, next) => {
     let token = req.get('token');
 
     jwt.verify(token, process.env.SEED, (err, decoded) => {
+
         if (err) {
-            return res.status(400).json({
+            return res.status(401).json({
                 ok: false,
-                err: { message: 'Token no valido' }
+                err: {
+                    message: 'Token no válido'
+                }
             });
-        };
+        }
 
         req.usuario = decoded.usuario;
         next();
+
     });
 
-    console.log(token);
-
 };
-
 
 /**
  * Verifica admin Role
@@ -30,16 +31,17 @@ let verificaAdmin_Role = (req, res, next) => {
 
     let usuario = req.usuario;
 
-    if (usuario.role != 'ADMIN_ROLE') {
+    if (usuario.role === 'ADMIN_ROLE') {
+        next();
+    } else {
+
         return res.json({
             ok: false,
-            err: 'El usuario no es administrador'
+            err: {
+                message: 'El usuario no es administrador'
+            }
         });
-    } else {
-        next();
-    };
-
-
+    }
 };
 
 module.exports = {
--- server/routes/login.js
+++ server/routes/login.js
@@ -15,14 +15,14 @@ app.post('/login', (req, res) => {
     }, (err, usuarioDB) => {
 
         if (err) {
-            return res.status(400).json({
+            return res.status(500).json({
                 ok: false,
                 err
             });
         }
 
         if (!usuarioDB) {
-            return res.status(500).json({
+            return res.status(400).json({
                 ok: false,
                 err: {
                     message: '(Usuario) o contraseña incorrectos'
@@ -31,7 +31,7 @@ app.post('/login', (req, res) => {
         }
 
         if (!bcrypt.compareSync(body.password, usuarioDB.password)) {
-            return res.status(500).json({
+            return res.status(400).json({
                 ok: false,
                 err: {
                     message: 'Usuario o (contraseña) incorrectos'
@@ -39,12 +39,10 @@ app.post('/login', (req, res) => {
             });
         }
 
-        //generamos el TOKEN
-        //, { expiresIn: segundos * minutos * horas * dias}
-        //, { expiresIn: 60 * 60 * 24 * 30}
+        //generamos token
         let token = jwt.sign({
             usuario: usuarioDB
-        }, 'este-es-el-secret-desarrollo', { expiresIn: 60 * 60 * 24 * 30 });
+        }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
 
         res.json({
             ok: true,
